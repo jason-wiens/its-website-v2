50b9ec8502ef9f0a46f0da6752a4dc9fb324a680
Jason Wiens <jwiens@integrity-tech.ca>
2020-05-21 13:57:24 -0600

    add website description

--- src/App.js
+++ src/App.js
@@ -3,23 +3,11 @@ import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
-class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      section: ''
-    }
-  }
-
-
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div className="App">
+    
+    
+  </div>
+)
 
 export default App;
